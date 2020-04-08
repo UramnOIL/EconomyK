3114c0f32fe0437c53767a02f757f2ec16cc9d49
UramnOIL <uramnoil@outlook.jp>
2020-04-08 15:24:04 +0900

    進捗

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 dependencies {
-    val implementation by configurations
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.exposed:exposed-core:0.23.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.23.1")
